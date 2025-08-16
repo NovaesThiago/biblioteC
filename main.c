d919ad92d14c5ebd70d9cfe10f41fe82543c1967
NovaesThiago <novaesthiagoss@gmail.com>
2025-08-16 01:50:09 -0300

    Adicionando menu interativo na main.c

--- main.c
+++ main.c
@@ -1,17 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "livro.h"
 
+void limparBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
 int main() {
-    cadastrarLivro("Dom Casmurro");
-    cadastrarLivro("O Pequeno Príncipe");
+    int opcao;
+    char nomeLivro[MAX_NOME];
+    char nomePessoa[MAX_NOME];
 
-    requisitarLivro("Dom Casmurro", "Ana");
-    requisitarLivro("Dom Casmurro", "Bruno");
-    requisitarLivro("Dom Casmurro", "Carlos");
+    while (1) {
+        printf("\n===== MENU BIBLIOTECA =====\n");
+        printf("1. Cadastrar livro\n");
+        printf("2. Requisitar livro\n");
+        printf("3. Devolver livro\n");
+        printf("4. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+        limparBuffer();
 
-    devolverLivro("Dom Casmurro");
-    devolverLivro("Dom Casmurro");
-    devolverLivro("Dom Casmurro");
+        switch (opcao) {
+            case 1:
+                printf("Nome do livro: ");
+                fgets(nomeLivro, MAX_NOME, stdin);
+                nomeLivro[strcspn(nomeLivro, "\n")] = '\0'; // Remove \n
+                cadastrarLivro(nomeLivro);
+                break;
+
+            case 2:
+                printf("Nome do livro: ");
+                fgets(nomeLivro, MAX_NOME, stdin);
+                nomeLivro[strcspn(nomeLivro, "\n")] = '\0';
+
+                printf("Nome da pessoa: ");
+                fgets(nomePessoa, MAX_NOME, stdin);
+                nomePessoa[strcspn(nomePessoa, "\n")] = '\0';
+
+                requisitarLivro(nomeLivro, nomePessoa);
+                break;
+
+            case 3:
+                printf("Nome do livro a devolver: ");
+                fgets(nomeLivro, MAX_NOME, stdin);
+                nomeLivro[strcspn(nomeLivro, "\n")] = '\0';
+
+                devolverLivro(nomeLivro);
+                break;
+
+            case 4:
+                printf("Encerrando o programa...\n");
+                exit(0);
+
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+        }
+    }
 
     return 0;
 }
